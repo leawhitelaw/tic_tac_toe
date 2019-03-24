0e76b64e070ea2facea227cfe180c6f1e91aa66e
leawhitelaw <40279124@live.napier.ac.uk>
2019-03-24 22:14:56 +0000

    added queue for replay

--- tic_tac_toe.c
+++ tic_tac_toe.c
@@ -3,6 +3,7 @@
 #include <ctype.h>
 #define MAX 9
 
+/*****define struct for recording each move*****/
 struct move{
 	char x;
 	char y;
@@ -15,39 +16,41 @@ struct stack
   int top;
 };
 
-// functions for tic-tac-toe
+/*****functions for game*****/
 char matrix[3][3];
 int player;
 void init_matrix();
 void get_player_move(int player, struct move *);
 void check_play(struct stack *, struct stack *);
 void display_move(struct move *);
-//void get_comp_move();
 void display_board();
 void undo();
 void redo();
 void replay();
-//void instructions();
+void instructions();
 char check_win();
 char check_draw();
+//void get_comp_move();
 
-//functions for stacks
+/*****functions for stacks*****/
 void init_stack(struct stack *);
 void push(struct stack *, struct move *);
 struct move *pop(struct stack *);
 
-//functions for queue
-void enqueue(int *, int, int *, int *);
-int dequeue(int *, int *, int *);
-int empty(int *); 
+/*****functions for queue*****/
+void enqueue(struct move *, struct move *, int *, int *);
+struct move *dequeue(struct move *, int *, int *);
 
 int main()
 {
-	//its showing all the moves that have been done, even when the stack is popped. If it
-	//was just showing 'move' it would show only 1 X or O
+	//init structs
 	struct stack s1;
 	struct stack s2;
 	struct move move_arr;
+	//init queues
+	struct move arr[MAX];
+  int front = -1, rear = -1;
+
 	init_stack(&s1);
 	init_stack(&s2);
 	player=1;
@@ -68,12 +71,13 @@ int main()
 		display_board();
 		check_play(&s1, &s2);
 		move_arr = s1.array[s1.top];
+		enqueue(arr, &move_arr, &front, &rear);
 		display_move(&move_arr);
 		printf("\n\nPlayer %d enter X,Y co-ords for your move: ", player);
 		get_player_move(player, &move_arr);
 		display_move(&move_arr);
 		push(&s1, &move_arr);
-		done = check_win(); //see if anyone has won
+		done = check_win();
 		done = check_draw();
 		if(done!=' ') break;
 		if(player==1){
@@ -96,11 +100,10 @@ int main()
 	}
 	//printf("HA! I have beaten you!\n");
 	display_board(); //show final positions
-
 	return 0;
 }
 
-//Initialise Matrix/Board
+/*****Initialise the board*****/
 void init_matrix()
 {
 	int i, j;
@@ -112,6 +115,8 @@ void init_matrix()
 		}
 	}
 }
+
+/*****display each move*****/
 void display_move(struct move *move_arr)
 {
 	if(move_arr->player == 1){
@@ -121,21 +126,16 @@ void display_move(struct move *move_arr)
 		matrix[move_arr->x][move_arr->y] = 'O';
 	}
 }
-//get move from player 1
+
+/*****get move from each player*****/
 void get_player_move(int player, struct move *move_arr)
 {
 	int x, y;
 
 	int check = scanf("%d%*c%d",&x, &y);
 	printf("check is %d:", check);
-	/*if(check!= 2){
+	/*if(check!= 2 || !(isdigit(x)) || !(isdigit(y))){
 		printf("Please enter integer X and Y co-ordinates in the format \"X,Y\"'.\n");
-		get_player_move(player, move_arr);
-	}else if(!(isdigit(x))){
-		printf("Please enter integer for X.\n");
-		get_player_move(player, move_arr);
-	}else if(!(isdigit(y))){
-		printf("Please enter integer for Y.\n");
 		get_player_move(player, move_arr);
 	}*/
 
@@ -152,6 +152,8 @@ void get_player_move(int player, struct move *move_arr)
 		move_arr->player = player;
 	}
 }
+
+/*****check for undo, redo, play or replay*****/
 void check_play(struct stack *s1, struct stack *s2)
 {
 	struct move move;
@@ -186,6 +188,8 @@ void check_play(struct stack *s1, struct stack *s2)
 	}
 	return;
 }
+
+/*****check if players have drawn*****/
 char check_draw()
 {
 	int i, j;
@@ -199,29 +203,9 @@ char check_draw()
 	}
 	return 'D';
 }
-/*void get_comp_move()
-{
-	int i, j;
-	for(i=0; i<3; i++)
-	{
-		for(j=0; j<3; j++)
-		{
-			if(matrix[i][j]==' ') break;
-		}
-		if(matrix[i][j]==' ') break;
-	}
 
-	if(i*j==9)
-	{
-		printf("Draw\n");
-		exit(0);
-	}
-	else
-	{
-		matrix[i][j] = 'O';
-	}
-}*/
-// Display board on screen
+
+/*****display game board*****/
 void display_board()
 {
 	int i;
@@ -235,7 +219,8 @@ void display_board()
 	}
 	printf("\n");
 }
-// check if someone has won
+
+/*****check if player has won*****/
 char check_win()
 {
 	int i;
@@ -266,10 +251,13 @@ char check_win()
 	return ' ';
 }
 
+/*****initialise stack*****/
 void init_stack(struct stack *s)
 {
   s->top = -1;
 }
+
+/*****push(add) to stack*****/
 void push(struct stack *s, struct move *item)
 {
   if(s->top == MAX -1)
@@ -280,6 +268,8 @@ void push(struct stack *s, struct move *item)
   s->top++;
   s->array[s->top] = *item;
 }
+
+/*****pop(remove) from stack*****/
 struct move *pop(struct stack *s)
 {
   struct move *data;
@@ -292,3 +282,58 @@ struct move *pop(struct stack *s)
   s->top--;
   return data;
 }
+
+/*****enqueue(add) to queue*****/
+void enqueue(struct move *arr, struct move *item, int *pfront, int *prear)
+{
+  if(*prear == MAX-1)
+  {
+    printf("Queue is full\n");
+    return;
+  }
+  else
+  {
+    (*prear)++;
+    arr[*prear] = *item;
+
+    if(*pfront == -1)
+    {
+      *pfront =0;
+    }
+  }
+}
+
+/*****dequeue(remove) from queue*****/
+struct move *dequeue(struct move *arr, int *pfront, int *prear)
+{
+  struct move *data = &arr[*pfront];
+
+//  arr[*pfront] = 0;
+  if(*pfront == *prear)
+    *pfront = *prear = -1;
+  else
+    (*pfront)++;
+  return data;
+}
+/*void get_comp_move()
+{
+	int i, j;
+	for(i=0; i<3; i++)
+	{
+		for(j=0; j<3; j++)
+		{
+			if(matrix[i][j]==' ') break;
+		}
+		if(matrix[i][j]==' ') break;
+	}
+
+	if(i*j==9)
+	{
+		printf("Draw\n");
+		exit(0);
+	}
+	else
+	{
+		matrix[i][j] = 'O';
+	}
+}*/
